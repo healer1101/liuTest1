35b6e94b547df5700588f120a7829385128db159
healer1101 <healer1101@163.com>
2022-11-21 17:39:58 +0800

    首次提交

---
+++ test1/src/main/java/com/liuzhy/Test1.java
@@ -0,0 +1,30 @@
+package com.liuzhy;
+
+/**
+ * @author: zhiyin.liu
+ * @create: 2022-11-21 5:35 PM
+ * @desc：
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        getId();
+    }
+
+    private static void getId() {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");
+        System.out.println("11");
+        System.out.println("12");
+        System.out.println("13");
+        System.out.println("14");
+        System.out.println("15");
+    }
+}
